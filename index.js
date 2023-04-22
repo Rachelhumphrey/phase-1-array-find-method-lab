8b224007f5c5fd63e61ac36348dad7413eec3d54
RachelHumphrey <rachelhumphrey87@gmail.com>
2023-04-22 12:17:37 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+const array = [
+    {year: "2015", result: "W"},
+    {year: "2014", result: "N/A"},
+    {year: "2013", result: "L"},
+]
+function superbowlWin(array, year) {
+   for (year of array) {
+    if (year.result === "W") {
+        return ("2015");
+    }
+   } 
+}
+superbowlWin(year, "2015");
